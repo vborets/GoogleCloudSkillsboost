76f47daa603d5861c40778fe963ae0df2745b801
Abhishek <abhishekrocks232@gmail.com>
2025-04-06 00:15:46 +0530

    Create abhishek.sh

---
+++ Monitor Environments with Google Cloud Managed Service for Prometheus: Challenge Lab/abhishek.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
+
+# Welcome message
+echo "${CYAN}${BOLD}"
+echo "Welcome to Dr. Abhishek Tutorials!"
+echo "Please do like, share and subscribe to our YouTube channel:"
+echo "https://www.youtube.com/@drabhishek.5460/videos"
+echo "${RESET}"
+
+export ZONE=$(gcloud compute project-info describe \
+--format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+
+gcloud container clusters create gmp-cluster --num-nodes=3 --zone=$ZONE
+
+gcloud container clusters get-credentials gmp-cluster --zone=$ZONE
+
+kubectl create ns gmp-test
+
+kubectl -n gmp-test apply -f https://raw.githubusercontent.com/GoogleCloudPlatform/prometheus-engine/v0.2.3/manifests/setup.yaml
+
+kubectl -n gmp-test apply -f https://raw.githubusercontent.com/GoogleCloudPlatform/prometheus-engine/v0.2.3/manifests/operator.yaml
+
+kubectl -n gmp-test apply -f https://raw.githubusercontent.com/GoogleCloudPlatform/prometheus-engine/v0.2.3/examples/example-app.yaml
+
+cat > op-config.yaml <<'EOF_END'
+apiVersion: monitoring.googleapis.com/v1alpha1
+collection:
+  filter:
+    matchOneOf:
+    - '{job="prom-example"}'
+    - '{__name__=~"job:.+"}'
+kind: OperatorConfig
+metadata:
+  annotations:
+    components.gke.io/layer: addon
+    kubectl.kubernetes.io/last-applied-configuration: |
+      {"apiVersion":"monitoring.googleapis.com/v1alpha1","kind":"OperatorConfig","metadata":{"annotations":{"components.gke.io/layer":"addon"},"labels":{"addonmanager.kubernetes.io/mode":"Reconcile"},"name":"config","namespace":"gmp-public"}}
+  creationTimestamp: "2022-03-14T22:34:23Z"
+  generation: 1
+  labels:
+    addonmanager.kubernetes.io/mode: Reconcile
+  name: config
+  namespace: gmp-public
+  resourceVersion: "2882"
+  uid: 4ad23359-efeb-42bb-b689-045bd704f295
+EOF_END
+
+export PROJECT=$(gcloud config get-value project)
+
+gsutil mb -p $PROJECT gs://$PROJECT
+
+gsutil cp op-config.yaml gs://$PROJECT
+
+gsutil -m acl set -R -a public-read gs://$PROJECT
+
+echo "${BG_RED}${BOLD}Do like the video and subscribe the channel !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
