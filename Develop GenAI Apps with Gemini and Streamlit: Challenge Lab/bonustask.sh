c3ff3043c8103f177d1fb5d0a1e67b91a116f562
Abhishek <abhishekrocks232@gmail.com>
2025-04-17 12:52:27 +0530

    Delete Develop GenAI Apps with Gemini and Streamlit: Challenge Lab directory

--- Develop GenAI Apps with Gemini and Streamlit: Challenge Lab/bonustask.sh
+++
@@ -1,9 +0,0 @@
-
-cd generative-ai/gemini/sample-apps/gemini-streamlit-cloudrun
-pip install google-cloud-aiplatform
-
-streamlit run chef.py \
-  --browser.serverAddress=localhost \
-  --server.enableCORS=false \
-  --server.enableXsrfProtection=false \
-  --server.port 8080
