a8b0e952cee396d9398c7597ac452b0481d6d5cd
Abhishek <abhishekrocks232@gmail.com>
2025-04-29 15:56:04 +0530

    Update abhishek.sh

--- BigQuery: Qwik Start - Command Line/abhishek.sh
+++ BigQuery: Qwik Start - Command Line/abhishek.sh
@@ -81,7 +81,9 @@ bq mk babynames
 echo ""
 
 echo -e "${BOLD_MAGENTA}→ Downloading and extracting baby names data...${RESET}"
+
 wget https://github.com/Itsabhishek7py/GoogleCloudSkillsboost/raw/refs/heads/main/BigQuery:%20Qwik%20Start%20-%20Command%20Line/names.zip
+
 unzip names.zip
 echo ""
 
