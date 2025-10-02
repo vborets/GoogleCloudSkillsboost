c64d339dc7cf0466dfe73206ca84af8306b71197
Abhishek <abhishekrocks232@gmail.com>
2025-10-03 02:26:02 +0530

    Update abhishek.sh

--- Arcade Hero Enter the Cloud Run functions Pub Sub/abhishek.sh
+++ Arcade Hero Enter the Cloud Run functions Pub Sub/abhishek.sh
@@ -1,10 +1,29 @@
-gcloud config set compute/region $REGION
+#!/bin/bash
+
+# Set your project
 export PROJECT_ID=$(gcloud config get-value project)
 
+# Set default region
+DEFAULT_REGION="us-central1"
 
+# Prompt user to enter region with default value
+echo "Enter your preferred region (default: $DEFAULT_REGION):"
+read REGION
+
+# Use default if no input provided
+REGION=${REGION:-$DEFAULT_REGION}
+
+echo "Using region: $REGION"
+
+# Set the region in gcloud config
+gcloud config set functions/region $REGION
+gcloud config set compute/region $REGION
+
+# Clone the repository and navigate to the function
 git clone https://github.com/GoogleCloudPlatform/nodejs-docs-samples.git
 cd nodejs-docs-samples/functions/v2/helloPubSub/
 
+# Deploy the function
 gcloud functions deploy cf-demo \
 --gen2 \
 --runtime=nodejs20 \
